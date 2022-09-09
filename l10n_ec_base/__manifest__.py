68a8da879e920bd7ad5595acf46e39138d7ecebd
sabiduriainformatica <sabiduriainformatica@gmail.com>
2022-09-08 20:38:19 -0500

    [IMP] l10n_ec_base: Valida Puntos Establecimiento y Emision

--- l10n_ec_base/__manifest__.py
+++ l10n_ec_base/__manifest__.py
@@ -1,56 +1,19 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'l10_ec_base',
-    'version': '1.1',
-    'summary': 'Invoices & Payments',
-    'sequence': 10,
-    'author': "Renan Nazate",
-    'description': """
-Invoicing & Payments
-====================
-The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
+    'name': "Validate Sucursal Point Emission",
 
-You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
-    """,
-    'category': 'Accounting/Localizations/Account Charts',
-    'depends': [
-        "base",
-        "base_iban",
-        "account_debit_note",
-        "l10n_latam_invoice_document",
-        "l10n_latam_base",
-    ],
+    'summary': """Validate Sucursal and Point Emission""",
+
+    'author': "Odoo Community Association (OCA), Renan Nazate",
+    'website': "https://github.com/OCA/l10n-ecuador",
+    'license': 'AGPL-3',
+    'version': '15.0.1.0.0',
+    'depends': ['account'],
+
     'data': [
-        # Chart of Accounts
-        "data/account_chart_template_data.xml",
-        # "data/account_group_template_data.xml",
-        "data/account.account.template.csv",
-        "data/account_chart_template_setup_accounts.xml",
-        # Taxes
-        "data/account_tax_group_data.xml",
-        "data/account_tax_report_data.xml",
-        # "data/account_tax_template_vat_data.xml",
-        # "data/account_tax_template_withhold_profit_data.xml",
-        # "data/account_tax_template_withhold_vat_data.xml",
-        "data/account_fiscal_position_template.xml",
-        # Partners data
-        "data/res.bank.csv",
-        "data/l10n_latam_identification_type_data.xml",
-        # "data/res_partner_data.xml",
-        # Other data
-        # "data/account_chart_template_configure_data.xml",
-        "data/l10n_latam.document.type.csv",
-        "data/l10n_ec.sri.payment.csv",
-        "views/account_tax_view.xml",
-        "views/l10n_latam_document_type_view.xml",
-        "views/l10n_ec_sri_payment.xml",
-        # "views/account_journal_view.xml",
-        "security/ir.model.access.csv",
+
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': True,
-    'post_init_hook': '_account_post_init',
-    'license': 'LGPL-3',
+    'demo': [],
+    'application': False,
+    'installable': True
+
 }
--- l10n_ec_base/models/account_journal.py
+++ l10n_ec_base/models/account_journal.py
@@ -1,59 +1,19 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models
+import re
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    l10n_ec_sri_payment_id = fields.Many2one(
-        "l10n_ec.sri.payment.method",
-        "SRI Payment Method",
-    )
-    # similar to internal_type of l10n_latam.document.type
-    # for domain's purpose on invoice view
-    l10n_latam_internal_type = fields.Selection(
-        [
-            ("invoice", "Invoices"),
-            ("debit_note", "Debit Notes"),
-            ("credit_note", "Credit Notes"),
-            ("liquidation", "Liquidation"),
-        ],
-        string="Use journal in",
-    )
+    @api.constrains('l10n_ec_entity', 'l10n_ec_emission')
+    def _constrains_l10n_ec_entity_emission(self):
+        for rec in self:
+            if rec.l10n_ec_entity:
+                if re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.l10n_ec_entity):
+                    raise ValidationError(_("The point of entity must contain only numbers"))
 
-    @api.onchange("type")
-    def _onchange_type(self):
-        res = super(AccountJournal, self)._onchange_type()
-        if self.type in ("sale", "purchase"):
-            if not self.l10n_latam_internal_type:
-                self.l10n_latam_internal_type = "invoice"
-        else:
-            self.l10n_latam_internal_type = False
-        return res
-
-    @api.model
-    def _search(
-        self,
-        args,
-        offset=0,
-        limit=None,
-        order=None,
-        count=False,
-        access_rights_uid=None,
-    ):
-        if self.env.context.get("l10n_latam_internal_type") and self.env.company.country_id.code == "EC":
-            internal_type = self.env.context.get("l10n_latam_internal_type")
-            if internal_type == "credit_note":
-                args.append(("l10n_latam_internal_type", "in", ("invoice", internal_type)))
-            elif internal_type in ("out_receipt", "in_receipt"):
-                args.append(("l10n_latam_use_documents", "=", False))
-            else:
-                args.append(("l10n_latam_internal_type", "=", internal_type))
-        return super(AccountJournal, self)._search(
-            args,
-            offset=offset,
-            limit=limit,
-            order=order,
-            count=count,
-            access_rights_uid=access_rights_uid,
-        )
+            if rec.l10n_ec_emission:
+                if re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.l10n_ec_emission):
+                    raise ValidationError(_("The point of emission must contain only numbers"))

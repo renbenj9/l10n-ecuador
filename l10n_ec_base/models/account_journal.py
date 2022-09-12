c509c7018cd22b803f1e40bda33805e4182d27d2
sabiduriainformatica <sabiduriainformatica@gmail.com>
2022-09-12 18:59:55 -0500

    [IMP] l10n_ec_base: Corrige que se ingresen solo numeros

--- l10n_ec_base/models/account_journal.py
+++ l10n_ec_base/models/account_journal.py
@@ -1,23 +1,19 @@
 import re
 
-from odoo import _, api, models
+from odoo import _, api, models, fields
 from odoo.exceptions import ValidationError
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    # ("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", rec.email)
-
     @api.constrains('l10n_ec_entity', 'l10n_ec_emission')
     def _constrains_l10n_ec_entity_emission(self):
         for rec in self:
             if rec.l10n_ec_entity:
-                if len(rec.l10n_ec_entity) < 3 or re.match(r"^[.*+\-?\\^${}()|[\]]", rec.l10n_ec_entity) \
-                    or re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.l10n_ec_entity):
+                if len(rec.l10n_ec_entity) < 3 or not rec.l10n_ec_entity.isnumeric():
                     raise ValidationError(_("Length less than 3 numbers or The point of entity must contain only numbers"))
 
             if rec.l10n_ec_emission:
-                if len(rec.l10n_ec_emission) < 3 or re.match(r"^[.*+\-?\\^${}()|[\]]", rec.l10n_ec_emission) \
-                    or re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.l10n_ec_emission):
+                if len(rec.l10n_ec_emission) < 3 or not rec.l10n_ec_emission.isnumeric():
                     raise ValidationError(_("Length less than 3 numbers or The point of emission must contain only numbers"))

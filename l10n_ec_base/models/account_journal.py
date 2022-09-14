2543fe2728f6f5e58eadc3464671ed363ee986aa
sabiduriainformatica <sabiduriainformatica@gmail.com>
2022-09-13 21:39:01 -0500

    [IMP] l10n_ec_base: Anadir secuencia

--- l10n_ec_base/models/account_journal.py
+++ l10n_ec_base/models/account_journal.py
@@ -5,18 +5,7 @@ from odoo.exceptions import ValidationError
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    bi_sequence_id = fields.Many2one(
-        comodel_name="ir.sequence", string="Entry Sequence", copy=False
-    )
-    bi_sequence_next_number = fields.Integer(
-        string="Next Sequence Number", copy=False, default=1
-    )
-    bi_refund_sequence_id = fields.Many2one(
-        comodel_name="ir.sequence", string="Credit Note Entry Sequence", copy=False
-    )
-    bi_refund_sequence_next_number = fields.Integer(
-        string="Credit Note Next Number", copy=False, default=1
-    )
+    sequence_init = fields.Integer(string="Sequence Init", default=1)
 
     @api.constrains("l10n_ec_entity", "l10n_ec_emission")
     def _constrains_l10n_ec_entity_emission(self):
@@ -41,54 +30,3 @@ class AccountJournal(models.Model):
                             "emission must contain only numbers"
                         )
                     )
-
-    def write(self, vals):
-        res = super(AccountJournal, self).write(vals)
-        if "bi_sequence_next_number" in vals and self.type in ("sale", "purchase"):
-            for rec in self:
-                if rec.bi_sequence_id:
-                    if (
-                        rec.bi_sequence_id.use_date_range is True
-                        and len(rec.bi_sequence_id.date_range_ids) >= 1
-                    ):
-                        for i in rec.bi_sequence_id.date_range_ids:
-                            if i.date_from <= self.write_date.date() <= i.date_to:
-                                i.sudo().write(
-                                    {
-                                        "number_next_actual": vals[
-                                            "bi_sequence_next_number"
-                                        ]
-                                    }
-                                )
-                    else:
-                        rec.bi_sequence_id.sudo().write(
-                            {"number_next_actual": vals["bi_sequence_next_number"]}
-                        )
-        if "bi_refund_sequence_next_number" in vals and self.type in (
-            "sale",
-            "purchase",
-        ):
-            for rec in self:
-                if rec.bi_sequence_id:
-                    if (
-                        rec.bi_refund_sequence_id.use_date_range is True
-                        and len(rec.bi_refund_sequence_id.date_range_ids) >= 1
-                    ):
-                        for i in rec.bi_refund_sequence_id.date_range_ids:
-                            if i.date_from <= self.write_date.date() <= i.date_to:
-                                i.sudo().write(
-                                    {
-                                        "number_next_actual": vals[
-                                            "bi_refund_sequence_next_number"
-                                        ]
-                                    }
-                                )
-                    else:
-                        rec.bi_refund_sequence_id.sudo().write(
-                            {
-                                "number_next_actual": vals[
-                                    "bi_refund_sequence_next_number"
-                                ]
-                            }
-                        )
-        return res
--- l10n_ec_base/models/account_move.py
+++ l10n_ec_base/models/account_move.py
@@ -1,54 +1,11 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from datetime import datetime, date
-from odoo.exceptions import UserError, ValidationError
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    is_draft = fields.Boolean(string='Is Draft', default=False)
-
-    def button_draft(self):
-        res = super(AccountMove, self).button_draft()
-        self.is_draft = True
-        return res
-
-    @api.depends('posted_before', 'state', 'journal_id', 'date')
-    def _compute_name(self):
-        for rec in self:
-            if rec.journal_id.type in ('sale', 'purchase') and rec.move_type in (
-            'in_invoice', 'in_refund', 'out_invoice', 'out_refund') and self.name in (False, '/'):
-                rec.name = '/'
-            else:
-                super(AccountMove, self)._compute_name()
-
-    def write(self, vals):
-        if self.is_draft == False:
-            if 'state' in vals:
-                if vals['state'] in ('posted'):
-                    if self.journal_id.type in ('sale', 'purchase'):
-                        if self.move_type in ('out_invoice', 'in_invoice', 'out_receipt', 'in_receipt'):
-                            if not self.journal_id.bi_sequence_id:
-                                raise ValidationError('Add Sequence In Journal')
-                            else:
-                                seq = self.journal_id.l10n_ec_entity + "-" + self.journal_id.l10n_ec_emission + "-" + self.journal_id.bi_sequence_id._next()
-                                vals['name'] = seq
-                                self.journal_id.sudo().write({'bi_sequence_next_number': self.env['ir.sequence'].search(
-                                    [('id', '=', self.journal_id.bi_sequence_id.id)]).number_next_actual})
-                        elif self.move_type in ('out_refund', 'in_refund'):
-                            if not self.journal_id.bi_refund_sequence_id:
-                                raise ValidationError('Add Refund/Credit Note Sequence In Journal')
-                            else:
-                                seq = self.journal_id.l10n_ec_entity + "-" + self.journal_id.l10n_ec_emission + "-" + self.journal_id.bi_refund_sequence_id._next()
-                                vals['name'] = seq
-                                self.journal_id.sudo().write({'bi_refund_sequence_next_number': self.env[
-                                    'ir.sequence'].search(
-                                    [('id', '=', self.journal_id.bi_refund_sequence_id.id)]).number_next_actual})
-                elif vals['state'] in ('draft', 'cancel'):
-                    if self.journal_id.type in ('sale', 'purchase'):
-                        for rec in self:
-                            self.name = rec.name
-
-        return super(AccountMove, self).write(vals)
+    def _get_ec_formatted_sequence(self, number=0):
+        if number < 2:
+            number = self.journal_id.sequence_init
+        return super(AccountMove, self)._get_ec_formatted_sequence(number)
